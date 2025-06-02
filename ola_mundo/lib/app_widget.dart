a3ef3669774258d4fbb97f26ffcd9d725faab092
fabiosabatinebock <fabiosabatinejuina@gmail.com>
2025-06-01 23:05:26 -0400

    Aula 14 de Flutter

---
+++ ola_mundo/lib/app_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:ola_mundo/app_controller.dart';
+import 'package:ola_mundo/home_page.dart';
+
+class AppWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: AppController.instance,      
+      builder: (context, child){
+      return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        brightness: AppController.instance.isDarkTheme ? Brightness.dark : Brightness.light,
+      ),
+      home: HomePage(), 
+      );  
+    });
+  }
+}
---
+++ ola_mundo/lib/main.dart
@@ -0,0 +1,7 @@
+import "package:flutter/material.dart";
+import "package:ola_mundo/app_widget.dart";
+
+
+main() {
+  runApp(AppWidget());
+}
